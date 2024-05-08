2c6c9babe46f015ec94c21185e9d487636c05e31
MOMIZICH <74951309+mmz-bin@users.noreply.github.com>
2024-05-08 17:35:05 +0900

    Fixed internal logic

--- src/PBEnhancer.h
+++ src/PBEnhancer.h
@@ -41,46 +41,37 @@ public:
         RELEASING     //離している間毎回
     };
 
-    PBEnhancer(uint8_t pin, uint8_t mode, uint32_t longThreshold=500, uint32_t doubleThreshold=100);
+    PBEnhancer(const uint8_t pin, const uint8_t mode, const uint32_t longThreshold=500, const uint32_t doubleThreshold=100);
 
     uint8_t getPin() const;
 
-    void registerCallback(Event type, CallbackFunc func);
-
-    void removeCallback(Event type);
+    void registerCallback(const Event type, const CallbackFunc func);
+    void removeCallback(const Event type);
 
     void update();  //状態を更新する
 
-    bool hasOccurred(Event type) const;
+    bool hasOccurred(const Event type) const;
 
 private:
-    void onPress(uint32_t now);
+    void onPress(const uint32_t now);
+    void onRelease(const uint32_t now);
+    void onRisingEdge(const uint32_t now);
+    void onFallingEdge(const uint32_t now);
 
-    void onRelease(uint32_t now);
+    void emit(const Event type);
+    void invoke(); // コールバック関数を呼び出す
 
-    void onRisingEdge(uint32_t now);
 
-    void onFallingEdge(uint32_t now);
+    const uint8_t PIN, PIN_MODE, LONG_THRESHOLD, DOUBLE_THRESHOLD;
+    static constexpr uint8_t NUM_OF_EVENTS = 8;
 
-    void invoke(Event type); // コールバック関数を呼び出す
+    uint32_t pressTime_, releaseTime_;
 
-    uint8_t getIndex(Event type) const;
-
-    uint8_t pin_;              //ピン番号
-    uint8_t mode_;             //接続モード(INPUT/INPUT_PULLUPを想定)
-    uint32_t longThreshold_;   //長押し判定をする時間
-    uint32_t doubleThreshold_; //ダブルクリック判定をする時間0
-
-    uint32_t pressTime_;       //押している時間
-    uint32_t releaseTime_;     //離している時間
-
-    bool isPressBak_;          //isPressの前回のポーリングのときの値(エッジ検出用)
-    bool isHandled_;           //判定されたかどうか
-    bool isDoubleClickWait_;   //ダブルクリック待ちかどうか
+    bool isPressBak_, isLongPressWait_, isDoubleClickWait_;
 
     byte hasOccurred_;
 
-    CallbackFunc callbacks_[8];
+    CallbackFunc callbacks_[NUM_OF_EVENTS];
 };
 
 #endif
